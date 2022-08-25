7d1327fb927016b5fcdc0e448fce03987963a750
ofplarsen <ofplarsen@gmail.com>
2022-08-25 17:03:25 +0200

    Finished task 3. Problems plotting graph

--- Oving1/Oppg3.py
+++ Oving1/Oppg3.py
@@ -1,62 +1,59 @@
+import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-class LinearRegressionModel:
+class NotLinearRegressionModel:
 
     def __init__(self):
-        self.W = torch.tensor([[0.0]], requires_grad=True)
-        self.b = torch.tensor([[0.0]], requires_grad=True)
+        self.W = torch.tensor(torch.rand(1, 1), requires_grad=True)
+        self.b = torch.tensor(torch.rand(1, 1), requires_grad=True)
 
     # Predictor
     def f(self, x):
-        return x @ self.W + self.b
+        return 20 * torch.sigmoid((x @ self.W + self.b)) + 31
 
     def loss(self, x, y):
-        return torch.mean(torch.square(self.f(x) - y))
+        return torch.nn.functional.mse_loss(self.f(x), y)
+
+    def sigmoid(self, z): #Didn't work
+        return 1 / (1 + np.exp(-z))
 
 
-def oppg3():
-    path = "resources/day_head_circumference.csv"
-    data = pd.read_csv(path, delimiter=',', header=None, names=['length', 'weight'])
 
-    data['length'].pop(0)
-    data['weight'].pop(0)
+path = "resources/day_head_circumference.csv"
+data = pd.read_csv(path, dtype='float')
 
-    x_data = data['length'].values.astype(float).tolist()
-    y_data = data['weight'].values.astype(float).tolist()
+y_train = data.pop('head circumference')
+x_train = torch.tensor(data.to_numpy(), dtype=torch.float).reshape(-1, 1)
+y_train = torch.tensor(y_train.to_numpy(), dtype=torch.float).reshape(-1, 1)
 
-    print(x_data)
-    print(y_data)
 
-    x_train = torch.tensor(x_data).reshape(-1, 1)
-    y_train = torch.tensor(y_data).reshape(-1, 1)
+model = NotLinearRegressionModel()
+n = 100000
+lr = 0.000001
+p = 1000
+# Optimizer W, b, and learning rate
+optimizer = torch.optim.SGD([model.W, model.b], lr)
 
-    model = LinearRegressionModel()
-    n = 1000000
-    lr = 0.0001
-    p = 1000
-    # Optimizer W, b, and learning rate
-    optimizer = torch.optim.SGD([model.W, model.b], lr)
+for epoch in range(n):
+    model.loss(x_train, y_train).backward()  # Computes loss gradients
+    if epoch % p == 0:
+        print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_train)))
+    optimizer.step()  # Adjusts W and /or b
+    optimizer.zero_grad() #Clears gradients for next step
 
-    for epoch in range(n):
-        model.loss(x_train, y_train).backward()  # Computes loss gradients
-        if epoch % p == 0:
-            print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_train)))
-        optimizer.step()  # Adjusts W and /or b
-        optimizer.zero_grad() #Clears gradients for next step
+print("\nFinal: ")
+print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_train)))
 
-    print("\nFinal: ")
-    print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_train)))
+# Plot
 
-    # Plot
-
-    plt.plot(x_train, y_train, 'o', label='$(x^{(i)},y^{(i)})$')
-    plt.xlabel('Length')
-    plt.ylabel('Weight')
-    x = torch.tensor([[torch.min(x_train)], [torch.max(x_train)]])
-    plt.plot(x, model.f(x).detach(), label='$\\hat y = f(x) = xW+b$')
-    plt.gcf().text(0.15, 0.93,"n: " + str(n) + ", lr: " + str(lr) + ", loss: " + str(model.loss(x_train,y_train)))
-    plt.legend()
-    plt.show()
+plt.plot(x_train, y_train, 'o', label='$(x^{(i)},y^{(i)})$')
+plt.xlabel('Day')
+plt.ylabel('head circumference')
+x = torch.arange(torch.min(x_train), torch.max(x_train), 1.0).reshape(-1, 1)
+plt.plot(x, model.f(x).detach(), label='$\\hat y = f(x) = xW+b$')
+plt.gcf().text(0.15, 0.93,"n: " + str(n) + ", lr: " + str(lr) + ", loss: " + str(model.loss(x_train,y_train)))
+plt.legend()
+plt.show()
--- Oving1/main.py
+++ Oving1/main.py
@@ -2,7 +2,7 @@ import torch
 import matplotlib.pyplot as plt
 import pandas as pd
 #import Oppg1
-import Oppg2
-
+#import Oppg2
+import Oppg3
 #Oppg1.oppg1()
 #Oppg2
