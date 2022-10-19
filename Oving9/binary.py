92d247773cda95dd511eecf35de94483808b68f3
ofplarsen <ofplarsen@gmail.com>
2022-10-19 13:19:01 +0200

    Working like a charm :P

--- Oving9/binary.py
+++ Oving9/binary.py
@@ -1,6 +1,7 @@
 import math
+import time
 from dataclasses import dataclass
-
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -15,20 +16,22 @@ class Gen:
 
 
 class Evolution():
-    def __init__(self):
+    def __init__(self, max = 256, size=10):
+        self.size = size
+        self.max = max
         self.generations = []
-        self.start_gen = np.random.randint(0, 256, size=10)
+        self.start_gen = np.random.randint(0, max, size=size)
         self.generations.append(self.start_gen)
-        self.target = np.random.randint(0, 256)
+        self.target = np.random.randint(0, max)
 
     def mutation(self, x: int):
-        rnd_int = np.random.randint(0, 256)
+        rnd_int = np.random.randint(0, self.max)
         return x ^ rnd_int
 
     def combine(self, x, y):
-        floor = np.random.randint(0, 1)
+        floor = np.random.randint(0, 2)
 
-        return math.floor((x + y) / 2) if floor == 1 else math.ceil((x + y) / 2)
+        return math.floor((x + y) / 2) if floor == 1 else math.ceil((x + y) / 3)
 
     def fitness(self, x: int):
         return -np.abs(x - self.target)
@@ -48,7 +51,7 @@ class Evolution():
     def get_new_gen(self, old_gen):
         new_gen = self.get_new_combination(old_gen)
 
-        for i in np.random.randint(0, 9, size=3):
+        for i in np.random.randint(0, self.size, size=math.floor(self.size/3)):
             new_gen[i] = self.mutation(new_gen[i])
 
         return new_gen
@@ -61,7 +64,7 @@ class Evolution():
             fitnesses = [self.fitness(x) for x in self.generations[i]]
             fs.append(fitnesses)
             # Pick 5 best numbers
-            gen = sorted(zip(fitnesses, self.generations[i]), reverse=True)[:5]
+            gen = sorted(zip(fitnesses, self.generations[i]), reverse=True)[:(math.floor(self.size/2))]
             if max(fitnesses) == 0:
                 print(self.generations)
                 return [Gen(max(f), sum(f)/len(f)) for f in fs], self.generations
@@ -72,6 +75,30 @@ class Evolution():
 
             i += 1
 
+    def train_time(self):
+        i = 0
+        sts = []
+
+        for t in range(3):
+            print(i)
+            st = time.time()
+            while True:
+
+                fitnesses = [self.fitness(x) for x in self.generations[i]]
+                # Pick 5 best numbers
+                gen = sorted(zip(fitnesses, self.generations[i]), reverse=True)[:(math.floor(self.size/2))]
+                if max(fitnesses) == 0:
+                    et = time.time()
+                    sts.append(et-st)
+                    break
+                print(gen)
+                new_gen = self.get_new_gen(gen)
+                self.generations.append(new_gen)
+
+                i += 1
+
+        return sum(sts)/len(sts)
+
 
 def print_gens(gen, best, avg, i):
     print(f"Gen {i}: ")
@@ -80,11 +107,32 @@ def print_gens(gen, best, avg, i):
     print(f"Avg Fitness: {avg}")
     print("")
 
-evo = Evolution()
+def task_1_1():
+    evo = Evolution()
 
-gen, gens = evo.train()
+    gen, gens = evo.train()
 
-for i in range(len(gens)):
-    print_gens(gens[i], gen[i].best_fitness, gen[i].avg_fitness, i)
+    for i in range(len(gens)):
+        print_gens(gens[i], gen[i].best_fitness, gen[i].avg_fitness, i)
 
-print(f"Target: {evo.target}")
+    print(f"Target: {evo.target}")
+
+def task_1_2():
+    n = np.arange(8, 19)
+    time  = []
+
+    for i in range(8, 19):
+        evo = Evolution(2**i, 10)
+        time.append(evo.train_time())
+        print(f"Done with: {2**i}")
+    print(len(time))
+    print(len(n))
+
+    plt.plot(n, time)
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    #task_1_1()
+    task_1_2()
